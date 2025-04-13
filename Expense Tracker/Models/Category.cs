f59ff5f322fa0bff490373b459563d495818077b
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-13 16:14:16 +0300

    added Transaction model and small changes in Category

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace Expense_Tracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; }
+        public string Icon { get; set; } = "";
+        public string Type { get; set; } = "Expense";
+
     }
 }
---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } //navigation propert
+        public double Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
