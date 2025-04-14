afe9d8d62d517480d9e7f1fe428211cdf057a3a3
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-14 21:09:32 +0300

    added category index action and corresponding view

---
+++ Expense Tracker/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Expense_Tracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        //here we need the db context
+        private readonly Context _context;
+
+        public CategoriesController(Context context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
