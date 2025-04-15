52d8de2031b85a09a687d58c485f03d07a60d6bd
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-15 09:23:08 +0300

    added index action and view

---
+++ Expense Tracker/Controllers/TransactionsController.cs
@@ -0,0 +1,21 @@
+﻿using Expense_Tracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        //here we need the dbContext
+        private readonly Context _context;
+        public TransactionsController(Context context)
+        {
+            _context = context;
+        }
+      public async Task<IActionResult> Index()
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+            return View(transactions);
+        }
+    }
+}
