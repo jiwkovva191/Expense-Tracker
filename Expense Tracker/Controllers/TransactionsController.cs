c49409a87a211c2028d2d0b465bcf1511251409d
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-15 09:44:26 +0300

    added create action and view

--- Expense Tracker/Controllers/TransactionsController.cs
+++ Expense Tracker/Controllers/TransactionsController.cs
@@ -1,5 +1,7 @@
-﻿using Expense_Tracker.Data;
+﻿using Expense_Tracker.Models;
+using Expense_Tracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Expense_Tracker.Controllers
@@ -16,6 +18,24 @@ namespace Expense_Tracker.Controllers
         {
             var transactions = await _context.Transactions.ToListAsync();
             return View(transactions);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Categories"] = new SelectList(_context.Categories, "CategoryId", "Title");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("TransactionId, CategoryId, Amount, Note, Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Transactions.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
         }
     }
 }
